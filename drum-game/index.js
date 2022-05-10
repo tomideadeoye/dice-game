30600328f8e83a95fd64a82d603de2e5fca5244a
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2022-05-10 17:48:07 +0100

    Add files via upload

---
+++ drum-game/index.js
@@ -0,0 +1,50 @@
+let body = document.querySelector("body");
+let playall = document.querySelector(".playall");
+let stop = document.querySelector(".stop");
+
+let soundArray = [
+	"crash",
+	"kick-bass",
+	"snare",
+	"tom-1",
+	"tom-2",
+	"tom-3",
+	"last",
+	"toms",
+];
+let audios = [];
+
+// for loop adding event listeners to all buttons
+for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+	document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+		console.log(event.target.innerHTML);
+		playSound(soundArray[i]);
+	});
+}
+
+// adding event listener to play all button
+playall.addEventListener("click", interval);
+
+function playSound(sound) {
+	// Random background color
+	body.style.backgroundColor =
+		"#" + Math.floor(Math.random() * 16777215).toString(16);
+	let audio = new Audio(`./sounds/${sound}.mp3`);
+	audio.play();
+}
+
+function interval() {
+	playList();
+}
+
+function playList() {
+	time = 1000;
+	playSound(soundArray[0]);
+
+	for (let i = 1; i < soundArray.length; i++) {
+		setTimeout(() => {
+			playSound(soundArray[i]);
+		}, time);
+		time = time + 500;
+	}
+}
